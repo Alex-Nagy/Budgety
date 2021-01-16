cf4e8efb6faefbbe2b90f65996a9c82339efe594
Alex <nagyal3x@gmail.com>
2021-01-16 17:32:00 +0100

    6/26 Dates

--- app.js
+++ app.js
@@ -165,7 +165,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
     
     var formatNumber = function (num, type) {
@@ -282,6 +283,17 @@ var UIController = (function() {
             })
         },
 
+        displayMonth: function() {
+            var now, month, year;
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -383,7 +395,9 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function(){
             console.log('Ali\'s budget app started');
-            
+
+            UICtrl.displayMonth();
+
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
